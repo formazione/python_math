380f31bd9b7277e4bd20676308bc5503a18be7b5
formazione <gatto.gio@gmail.com>
2020-09-27 21:29:36 +0200

    commented proportions.py

--- 001_proportions.py
+++ 001_proportions.py
@@ -7,13 +7,17 @@ class Prop:
         ie: 12 : 3 = 36:9
         '''
         self.proportion = prop
+        # replace = with :
         prop = prop.replace("=", ":")
-        self.prop = e1, m1, m2, e2 = prop.split(":")
+        # create a list with the numbers, splitting by the ':'
+        self.prop = prop.split(":")
+        # converts strings into integers and x to None
         for n, x in enumerate(self.prop):
             if x.strip() == "x":
                 self.prop[n] = None
             else:
                 self.prop[n] = int(x)
+        # memorize in memorable names the extrem and medium terms
         e1, m1, m2, e2 = self.prop
         self.e1 = e1
         self.e2 = e2
@@ -23,6 +27,7 @@ class Prop:
     def calculate_x(self):
         "Call this from the istance to find the incognito"
         print(f"The result of '{self.proportion}' is ", end="")
+        # Depending on what is None (incognito), it finds it with the other 3
         if self.e1 is None:
             res = self.m1 * self.m2 / self.e2
             return res
@@ -38,6 +43,7 @@ class Prop:
         print(res)
 
 
-
+# First you create the istance of Prop with a string containing the proposition
 p1 = Prop("12 : 3 = 36 : x")
+# Then you get the solution with calculate_x (method of Prop class)
 print(p1.calculate_x())
